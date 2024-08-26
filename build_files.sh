41a16c24781719f11562b257ef170394e2f13e28
Saif Ali <93928356+Saif-ali-shekh@users.noreply.github.com>
2024-08-26 12:37:14 +0530

    fixing varcel deployemt This Serverless Function has crashed

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-
-
 echo "BUILD START"
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+python3.12 -m pip install -r requirements.txt || { echo "pip install failed"; exit 1; }
+python3.12 manage.py collectstatic --noinput --clear || { echo "collectstatic failed"; exit 1; }
 echo "BUILD END"
